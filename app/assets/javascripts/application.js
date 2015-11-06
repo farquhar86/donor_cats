033ab9cfcecaf212dc834c2a88cb9418f221e22f
sswbelser <sswbelser@gmail.com>
2015-11-06 14:29:35 -0800

    Added client side validations to signup/login, moved files around as needed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap.min
+//= require jquery.validate
+//= require jquery.validate.additional-methods
 //= require_tree .

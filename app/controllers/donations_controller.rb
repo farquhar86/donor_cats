ee21e31407cbb95bdd06bef32526059a9c231a92
sswbelser <sswbelser@gmail.com>
2015-11-06 16:26:46 -0800

    Added basic donations controller

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,11 @@
+class DonationsController < ApplicationController
+  def new
+  	
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
-
 	root 'welcome#index'
 
 	resources :users, only: [:create]
 	get "/signup", to: "users#new", as: :signup
 	get "/profile", to: "users#show", as: :profile
 
-	resources :charities
-
 	resources :sessions, only: [:create]
 	get "/login", to: "sessions#new", as: :login
 	get "/logout", to: "sessions#destroy", as: :logout
+
+	resources :charities
+	resources :donations, only: [:new, :create, :show]
 
 end
 

06cfb794cf5f52ea06408935f4d1588922a4ca24
farquhar86 <farquharbradley@gmail.com>
2015-11-10 13:33:52 -0800

    stuff

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,7 +1,15 @@
 class DonationsController < ApplicationController
   def new
-
+  	@donation = Donation.new
   	
 
   end
+    def create
+        donation_params = params.require(:donation).permit(:amount)
+        byebug
+	    @donation = Donation.new(user_params)
+	    @donation.save
+	    redirect_to root_path
+	    
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
 
 	resources :charities
-
-
+  get '/:name/donations/new', to:"donations#new", as: :make_donation
+  post '/donations', to:"donations#create"
 	resources :sessions, only: [:create]
 	get "/login", to: "sessions#new", as: :login
 	get "/logout", to: "sessions#destroy", as: :logout
@@ -18,20 +18,22 @@ Rails.application.routes.draw do
 
 end
 
-#         root GET    /                             welcome#index
-#        users GET    /users(.:format)              users#index
-#              POST   /users(.:format)              users#create
-#     new_user GET    /users/new(.:format)          users#new
-#    edit_user GET    /users/:id/edit(.:format)     users#edit
-#         user GET    /users/:id(.:format)          users#show
-#              PATCH  /users/:id(.:format)          users#update
-#              PUT    /users/:id(.:format)          users#update
-#              DELETE /users/:id(.:format)          users#destroy
-#    charities GET    /charities(.:format)          charities#index
-#              POST   /charities(.:format)          charities#create
-#  new_charity GET    /charities/new(.:format)      charities#new
-# edit_charity GET    /charities/:id/edit(.:format) charities#edit
-#      charity GET    /charities/:id(.:format)      charities#show
-#              PATCH  /charities/:id(.:format)      charities#update
-#              PUT    /charities/:id(.:format)      charities#update
-#              DELETE /charities/:id(.:format)      charities#destroy
+#          root GET    /                              welcome#index
+#         users POST   /users(.:format)               users#create
+#        signup GET    /signup(.:format)              users#new
+#       profile GET    /profile(.:format)             users#show
+#     charities GET    /charities(.:format)           charities#index
+#               POST   /charities(.:format)           charities#create
+#   new_charity GET    /charities/new(.:format)       charities#new
+#  edit_charity GET    /charities/:id/edit(.:format)  charities#edit
+#       charity GET    /charities/:id(.:format)       charities#show
+#               PATCH  /charities/:id(.:format)       charities#update
+#               PUT    /charities/:id(.:format)       charities#update
+#               DELETE /charities/:id(.:format)       charities#destroy
+# make_donation GET    /:name/donations/new(.:format) donations#new
+#      sessions POST   /sessions(.:format)            sessions#create
+#         login GET    /login(.:format)               sessions#new
+#        logout GET    /logout(.:format)              sessions#destroy
+#     donations POST   /donations(.:format)           donations#create
+#  new_donation GET    /donations/new(.:format)       donations#new
+#      donation GET    /donations/:id(.:format)       donations#show
